5288be6e793e4bbdc7ef89756916632dda4fe681
KhaQuy <KhaQuy.khaquy1611@gmail.com>
2024-09-10 22:04:38 +0700

    make role login user

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Role;
+use App\Http\Middleware\RevalidateBackHistory;
+use App\Http\Middleware\RedirectIfAuthenticated;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias([
+            'role' => Role::class,
+            'preventBackHistory' => RevalidateBackHistory::class,
+            'redirectIfAuth' => RedirectIfAuthenticated::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Backend\Auth\AdminController;
+use App\Http\Controllers\Backend\Auth\AgentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::middleware(['auth', 'role:admin', 'preventBackHistory', 'redirectIfAuth'])->group(function() {
+    Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.index');
+});
+
+Route::middleware(['auth', 'role:agent', 'preventBackHistory', 'redirectIfAuth'])->group(function() {
+Route::get('agent/dashboard', [AgentController::class, 'index'])->name('agent.index');
 });
