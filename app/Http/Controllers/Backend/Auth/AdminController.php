dcbaac4c443e2b306e8a7621598dbc44da881834
KhaQuy <KhaQuy.khaquy1611@gmail.com>
2024-09-11 21:22:17 +0700

    admin module logout

--- app/Http/Controllers/Backend/Auth/AdminController.php
+++ app/Http/Controllers/Backend/Auth/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -14,5 +15,13 @@ class AdminController extends Controller
 
     public function index() {
         return view('backend.admin.dashboard.home.index');
+    }
+
+
+    public function logout(Request $request) {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('login');
     }
 }
